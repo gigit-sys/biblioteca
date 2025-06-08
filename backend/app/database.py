4df1392e0aae8cb639423b73428ce2223692a123
Luigi <ltudisco78@gmail.com>
2025-06-08 21:13:09 +0200

    Fix definitivo connessione PlanetScale via SSL

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
+import ssl
+
+ssl_args = {"ssl": {"ssl_ca": "/etc/ssl/certs/ca-certificates.crt"}}
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"ssl": {}}
+    connect_args=ssl_args
 )
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
